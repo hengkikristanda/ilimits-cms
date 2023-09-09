19bf0d4edbaf0a87085a830428b779c925e34de8
febeindri <febewednes@gmail.com>
2023-09-09 15:18:08 +0700

    Enable Save button

---
+++ js/controller/route.js
@@ -0,0 +1,20 @@
+const apiUrl = "http://localhost:3000";
+
+document.getElementById("saveButton").addEventListener("click", () => {
+	const content = tinymce.activeEditor.getContent();
+	fetch(`${apiUrl}/save`, {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: JSON.stringify({ content }),
+	})
+		.then((response) => response.json())
+		.then((data) => {
+			if (data.success) {
+				alert("Content saved successfully");
+			} else {
+				alert("Error saving content");
+			}
+		});
+});
